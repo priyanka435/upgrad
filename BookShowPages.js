2d41164dc8e694734de17235e6d9f0caa122ceb9
priyanka435 <67841015+priyanka435@users.noreply.github.com>
2024-01-31 13:15:04 +0530

    Create BookShowPages.js

---
+++ BookShowPages.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BookShowPage = () => {
+  return (
+    <div>
+      <h1>Book Show Page</h1>
+      {/* Content for booking shows */}
+    </div>
+  );
+};
+
+export default BookShowPage;
